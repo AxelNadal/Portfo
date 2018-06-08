02812aad58bc9fbf7ec244b96b11e69be89dd2cb
Axel Nadal <axel7m3s@gmail.com>
2018-06-08 16:23:14 +0200

    first commit

---
+++ resources/views/admin/contact/index.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::page')
+
+@section('title', 'contacts')
+
+@section('content_header')
+    <h1>Contact</h1>
+@stop
+
+@section('content')
+<div class="custom-file">
+        <input type="file" class="custom-file-input" id="customFile">
+        <label class="custom-file-label" for="customFile">Choose file</label>
+      </div>
+@stop
---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,9 @@
+@extends('welcome.contact', ['title'=>contact])
+
+@section('content')
+    <div class="container">
+        <h2>
+            Contact
+        </h2>
+    </div>
+@stop
